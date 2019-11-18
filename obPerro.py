1b4ea920ee75aaf3968607fbdc347e0b0ec281d2
Korlat <amaia.berrocal@gmail.com>
2019-11-18 14:27:55 +0100

    Crear una subclase o herencia de nuestra clase perro.

--- obPerro.py
+++ obPerro.py
@@ -1,5 +1,5 @@
 class Perro(): #Por convenio el nombre de la clase va siempre en mayúsculas.
-    def _init_(self, nombre, edad, peso): #Siempre empieza con esta función constructora.
+    def __init__(self, nombre, edad, peso): #Siempre empieza con esta función constructora.
         self.nombre = nombre
         self.edad = edad
         self.peso = peso
@@ -12,5 +12,31 @@ class Perro(): #Por convenio el nombre de la clase va siempre en mayúsculas.
             print ("guau, guau")
     # para llamar a esta función hay que nombrar primero a la instancia: salchicho.ladrar()
     
-    def _str_(self):
+    def __str__(self):
         return "Perro {}, edad: {}, peso: {}".format(self.nombre, self.edad, self.peso)
+    
+class PerroAsistencia(Perro): #Así decimos que la clase PerroAsistencia hereda de la clase Perro
+    def __init__(self, nombre, edad, peso, amo):
+        Perro.__init__(self, nombre, edad, peso)
+        self.amo = amo
+        self.__trabajando = False
+
+    def __str__(self): #Sobreescribir un método
+        return "Perro de asistencia {}, edad: {}, peso: {}, amo: {}".format(self.nombre, self.edad, self.peso, self.amo)
+
+    def pasear(self):
+            print("Ayudo a mi dueño {} a pasear".format(self.amo))
+            
+    def ladrar(self):
+        if self.__trabajando:
+            print("No puedo ladrar.")
+        else:
+            Perro.ladrar(self)
+            
+    def trabajando(self, valor = None):
+        if valor == None:
+            return self.__trabajando
+        else:
+            self.__trabajando = valor
+    
+    

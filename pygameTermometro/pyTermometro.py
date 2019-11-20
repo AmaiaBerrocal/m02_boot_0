abcb49d3c90db6c9968618f23af43aa86a4737c4
Korlat <amaia.berrocal@gmail.com>
2019-11-20 16:18:58 +0100

    Creación de la clase termómetro

--- pygameTermometro/pyTermometro.py
+++ pygameTermometro/pyTermometro.py
@@ -1,6 +1,10 @@
 import pygame, sys
 from pygame.locals import *
 
+class Termometro():
+    def __init__(self):
+        self.custome = pygame.image.load("")#poner ruta hacia la imagen elegida en la planificación
+    
 class MainApp():
     termometro = None
     entrada = None
@@ -10,6 +14,8 @@ class MainApp():
         self.__screen = pygame.display.set_mode((290, 415))
         pygame.display.set_caption("Termómetro")
         self.__screen.fill((244, 236, 203))
+        
+        self.termometro = Termometro()
 
     def __on_close(self):
         pygame.quit()
@@ -21,6 +27,7 @@ class MainApp():
                 if event.type == QUIT:
                     self.__on_close()
                     
+            self.__screen.blit(self.termometro.custome, (50, 34)) #coordenadas elegidas en la planificación
             pygame.display.flip()
             
 
